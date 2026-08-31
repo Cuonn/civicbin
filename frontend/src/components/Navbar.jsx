8c9c5fcff92478505fb70202b43bc4c7cf181d1e
Quan Tran <anhquan.tran203@gmail.com>
2026-08-31 22:33:28 +1000

    Polish: Add slogan into Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -26,7 +26,16 @@ const Navbar = () => {
           <path d="M11 20A7 7 0 0 1 9.8 6.1C15.5 5 17 4.48 19 2c1 2 2 4.18 2 8 0 5.5-4.78 10-10 10Z" />
           <path d="M2 21c0-3 1.85-5.36 5.08-6C9.5 14.52 12 13 13 12" />
         </svg>
-        CivicBin
+        <div className='flex flex-col'>
+          <span className='text-2x1 font-bold leading-tight'>
+            CivicBin
+          </span>
+          <span className='text-xs font-normal text-blue-100'>
+            Keep our streets clean, together
+          </span>
+
+        </div>
+        
       </Link>
       <div>
         {user ? (
